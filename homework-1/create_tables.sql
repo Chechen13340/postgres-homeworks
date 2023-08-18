ec3d76cddfef54f536c814d63425647f2d1b0b05
Artem Osipov <tema.osipov@mail.ru>
2023-08-18 20:15:11 +0300

    added listing code for created tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name text NOT NULL,
+	last_name text NOT NULL,
+	title text NOT NULL,
+	birth_date text NOT NULL,
+	notes varchar
+)
+
+CREATE TABLE customers
+(
+	customer_id text PRIMARY KEY,
+	company_name text NOT NULL,
+	contact_name text NOT NULL
+
+)
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id text NOT NULL,
+	employee_id int NOT NULL,
+	order_date text NOT NULL,
+	ship_city text NOT NULL
+
+)
+
+SELECT * FROM customers;
